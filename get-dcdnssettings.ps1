5581df439f0090bf4368b69b4de4d50a3534c808
Charles Betancourt <chuckbeta@users.noreply.github.com>
2018-11-08 16:28:07 -0500

    Update get-dcdnssettings.ps1

--- get-dcdnssettings.ps1
+++ get-dcdnssettings.ps1
@@ -1,3 +1,8 @@
+#Needed a way to grab all the DNS client settings on the DCs for a project where we were moving external DNS providers
+#The ops team was still logging into servers to grab the config and the SCCM team was using wmi calls
+#The below worked to get the info required. Problem was I didn't have permissions across entire forest, so needed to run on 
+#root domain
+
 #Gets all DCs in Forest
 $Forest = [System.DirectoryServices.ActiveDirectory.Forest]::GetCurrentForest()
 
@@ -5,4 +10,4 @@ $Forest = [System.DirectoryServices.ActiveDirectory.Forest]::GetCurrentForest()
 $dc = $Forest.Sites | % { $_.Servers } | Select Name,Domain
 
 #Queries each DC for DNS Server
-$dc | ForEach-Object {Invoke-Command -ComputerName $_.SamAccountName -ScriptBlock {Get-NetAdapter | Get-DnsClientServerAddress}} | Select-Object -Property PSComputerName,ServerAddresses | Where-Object {$_.ServerAddresses -ne ''}
+$dc | ForEach-Object {Invoke-Command -ComputerName $_.SamAccountName -ScriptBlock {Get-NetAdapter | Get-DnsClientServerAddress}} | Select-Object -Property PSComputerName,ServerAddresses | Where-Object {$_.ServerAddresses -ne ''} | export-csv .\dc_dnssettings.csv -nti
